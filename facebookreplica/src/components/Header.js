56eee7d839925601e2e957960c5b9ca508b443f1
Tlou <tramotshela@xds.co.za>
2023-11-22 16:45:49 +0200

    First page created nyana

---
+++ facebookreplica/src/components/Header.js
@@ -0,0 +1,14 @@
+import PlusOutlined from '@ant-design/icons'
+import '../components/components.css'
+
+const Header = () =>
+{
+  return (
+    <div className='title-div'>
+      <h1 className='title'>facebook</h1>
+      <div><PlusOutlined /></div>
+      
+    </div>
+  );
+}
+export default Header;
